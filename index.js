ef9134db2c42d89bebaca7685f315232b9c39794
name <cometotwinle@gmail.com>
2024-02-03 14:28:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    date= new Date(); //object of date()
+    hr = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    hr_rotation = 30 * hr + min / 2;//converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+    
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+    }, 1000);
